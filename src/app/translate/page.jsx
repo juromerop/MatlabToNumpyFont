6566e0eb611b159d859663188e31fad7254a760a
Joe Mendez <54820626+jzmendezl@users.noreply.github.com>
2023-11-25 22:19:08 -0500

    fix url

--- src/app/translate/page.jsx
+++ src/app/translate/page.jsx
@@ -10,7 +10,7 @@ export default function Home() {
 
   const handleTranslate = async () => {
     try {
-      const response = await axios.post("http://127.0.0.1:8000/trasnlate", {
+      const response = await axios.post("http://127.0.0.1:8000/translate", {
         text: inputCode,
       });
       console.log(response);
